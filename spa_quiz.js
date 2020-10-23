bdf379b49fd477dffd2970fe53a0131d9c320077
dveliz10 <70672340+dveliz10@users.noreply.github.com>
2020-10-23 17:34:53 -0400

    Update spa_quiz.js

--- spa_quiz.js
+++ spa_quiz.js
@@ -1,2 +1,16 @@
 document.addEvent Listener('DOMContentLoaded', () => {
   
+const render_view = (view) => {
+
+  template_source = document.querySelector(view.innerHTML)
+
+  var template = Handlebars.compile(template_source);
+
+var model = {
+  first_name : 'Daniel'
+}
+  console.log(model)
+  var html_widget_element = template(model)
+console.log(html_widget_element)
+  return html_widget_element
+}
